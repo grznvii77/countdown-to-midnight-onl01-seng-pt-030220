28d8673e13fc2fabad4a4e52117bda014b298ea0
Alina Jiang <alina.jiang95@gmail.com>
2020-03-08 06:12:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,11 @@ def countdown(seconds_remaining)
   "HAPPY NEW YEAR!"
 end
 
+
+def countdown(seconds_remaining)
+  while seconds_remaining > 0 
+    puts "#{seconds_remaining} SECOND(S)!"
+    sleep(1)
+    seconds_remaining -= 1 
+  end
+end
